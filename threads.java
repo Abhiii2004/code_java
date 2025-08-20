803b9579c6cf5dda37fcf579677fa9f4e520c180
Abhiii2004 <abhilashas200430@gmail.com>
2025-08-20 19:02:34 +0530

    Practice programs of Java basics

--- threads.java
+++ threads.java
@@ -1,22 +1,34 @@
 class MyThread extends Thread {
-    public void rn() {
-        while (true) {
-            System.out.println("cooking");
-        }
-    }}
-    class MyThread1 extends Thread {
-        public void rn() {
-            int i = 0;
-            while (i < 300) {
-                System.out.println("listening to podcast");
-                i++;
-            }
-        }}
-    public class threads {
-        public static void main(String[] args) {
-            MyThread t0 = new MyThread();
-            MyThread1 t1 = new MyThread1();
-            //t0.rn();
-            t1.rn();
+    public void run() {
+        System.out.println("cooking");
+    }
+}
+
+class MyThread1 extends Thread {
+    public void run() {
+        int i = 0;
+        while (i < 300) {
+            System.out.println("listening to podcast");
+            i++;
         }
     }
+}
+class MyThreadRunnable implements Runnable {
+    public void run() {
+        System.out.println("My thread using runnable");
+    }
+}
+
+public class threads {
+    public static void main(String[] args) {
+        MyThread t0 = new MyThread();
+        MyThread1 t1 = new MyThread1();
+
+        t0.start();
+        t1.start();
+
+        MyThreadRunnable t3 = new MyThreadRunnable();
+        Thread t2 = new Thread(t3);
+        t2.start();
+    }
+}
